6f0bdaf61b837d5efd6acb7e975ee209d84c5bbc
成杰 <huanghui@zailingtech.com>
2020-07-07 11:23:40 +0800

    update 2020/7/7

--- leetcode.py
+++ leetcode.py
@@ -24,38 +24,97 @@
 # print(res)
 
 # leetcode378
+# from typing import List
+# class Solution:
+#     def kthSmallest(self, matrix: List[List[int]], k):
+#         n = len(matrix)
+#         def check_mid(mid):
+#             i, j = n-1, 0  # i 表示行，j表示列
+#             cnt = 0
+#             while i >=0 and j <= n-1:
+#                 if matrix[i][j] <= mid:
+#                     cnt += i + 1   # 加i的原因是最后一列的数肯定是最大的，因此小于mid的个数可以直接加行数
+#                     j += 1
+#                 else:
+#                     i -= 1
+#             return cnt >= k
+#
+#         # 确定二分法的两个区间
+#         left, right = matrix[0][0], matrix[n-1][n-1]
+#         while left < right:
+#             mid = (left + right) // 2
+#             if check_mid(mid):
+#                right = mid
+#             else:
+#                 left = mid + 1
+#         return left
+#
+#
+# matrix = [
+#              [1, 5, 9],
+#              [10, 11, 13],
+#              [12, 13, 15]
+#          ]
+#
+# S = Solution()
+# res = S.kthSmallest(matrix, k=8)
+# print(res)
 from typing import List
+import collections
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+def create_Bitree(data, index):
+    Pnode = None
+    if index < len(data):
+        if data[index]  is  None:
+            return Pnode
+        Pnode = TreeNode(data[index])
+        Pnode.left = create_Bitree(data, 2*index + 1)
+        Pnode.right = create_Bitree(data, 2 * index + 2)
+
+    return Pnode
+
+# data = [5,4,8,11,None,13,4,7,2,None,None,None,1]
+# for i in range(len(data)):
+#     Pnode = create_Bitree(data, 0)
+# print(Pnode.left.val)
+
+
 class Solution:
-    def kthSmallest(self, matrix: List[List[int]], k):
-        n = len(matrix)
-        def check_mid(mid):
-            i, j = n-1, 0  # i 表示行，j表示列
-            cnt = 0
-            while i >=0 and j <= n-1:
-                if matrix[i][j] <= mid:
-                    cnt += i + 1   # 加i的原因是最后一列的数肯定是最大的，因此小于mid的个数可以直接加行数
-                    j += 1
-                else:
-                    i -= 1
-            return cnt >= k
+    def hasPathSum(self, root: TreeNode, sum: int) -> bool:
+        if not root:  # 可能存在树为空的情况
+            return False
+        que_node = collections.deque([root])   # 记录遍历的节点
+        que_val = collections.deque([root.val])  # 记录遍历节点的value
+        while que_node:
+            now = que_node.popleft()
+            temp_val = que_val.popleft()
+            if not now.left and not now.right:   # 处于叶子节点位置判断与目标值的关系
+                if temp_val == sum:
+                    return True
+                continue
 
-        # 确定二分法的两个区间
-        left, right = matrix[0][0], matrix[n-1][n-1]
-        while left < right:
-            mid = (left + right) // 2
-            if check_mid(mid):
-               right = mid
-            else:
-                left = mid + 1
-        return left
+            if now.left:
+                que_node.append(now.left)
+                que_val.append(now.left.val + temp_val)
 
+            if now.right:
+                que_node.append(now.right)
+                que_val.append(now.right.val + temp_val)
+        return False
 
-matrix = [
-             [1, 5, 9],
-             [10, 11, 13],
-             [12, 13, 15]
-         ]
+data = [5,4,8,11,None,13,4,7,2,None,None,None,1]
 
+Pnode = create_Bitree(data, 0)
+# print(Pnode.left.left.val)
+sum = 22
 S = Solution()
-res = S.kthSmallest(matrix, k=8)
-print(res)
+out = S.hasPathSum(Pnode, sum)
+print(out)
+
---
+++ leetcode/树/create_tree.md
@@ -0,0 +1,48 @@
+# 构建二叉树
+
+## 0. 前言
+&emsp;&emsp;在leetcode官网的编辑器中自动会生成测试用例， [如题](https://leetcode-cn.com/problems/path-sum/)
+输入数据为List,如果本地的IDE中肯定无法直接使用List作为输入数据，这也是很多公司在做笔试编程时碰到的第一个问题，对输入
+数据的构建。  
+&emsp;&emsp;在leetcode中提供了如下的code定了tree类，但是此代码只是完成了对树的定义，没有能够从List完成对而树的构建。
+本文对此进行解释说明。
+
+    # Definition for a binary tree node.
+     class TreeNode:
+        def __init__(self, x):
+           self.val = x
+           self.left = None
+           self.right = None
+
+## 1. 构建二叉树
+&emsp;&emsp;以此`[5,4,8,11,null,13,4,7,2,null,null,null,1]`作为测试用例来构建二叉树，由于二叉树的特殊性，更加容易实现。
+`List[0]`肯定是作为树的根节点，后面的元素两两组队最为前一个的左右子树，进一步理解列表第1、第2个元素是作为第0个元素的左右子树，第3、第4个元素
+是作为第1个元素的左右子树，因此list中当前位置处元素左右子树分别位于`2*index+1、 2*index+2`处。
+
+&emsp;&emsp;代码如下：
+
+    class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+    def create_Bitree(data, index):
+        Pnode = None
+        if index < len(data):
+            if data[index]  is  None:
+                return Pnode
+            Pnode = TreeNode(data[index])
+            Pnode.left = create_Bitree(data, 2*index + 1)
+            Pnode.right = create_Bitree(data, 2 * index + 2)
+    
+        return Pnode
+    
+    data = [5,4,8,11,None,13,4,7,2,None,None,None,1]
+    for i in range(len(data)):
+        Pnode = create_Bitree(data, 0)
+    print(Pnode.left.val)
+    
+    >>> 4 ## 根节点的左子树的值
+
