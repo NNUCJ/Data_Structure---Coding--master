224d43af69015dd13a2f235b764c9a6bdba0fcab
NNUCJ <38729815+NNUCJ@users.noreply.github.com>
2020-07-11 12:27:30 +0800

    动态规划/股票问题

--- leetcode.py
+++ leetcode.py
@@ -134,3 +134,17 @@ import collections
 # S =Solution()
 # res = S.divingBoard(1, 1, 0)
 # print(res)
+class Solution:
+    def maxProfit(self, prices):
+        if not prices:
+            return 0
+
+        n = len(prices)
+        dp = [[-prices[0], 0, 0]] + [[0] * 3 for _ in range(n - 1)]  # 构建dp数组存储状态
+
+        for i in range(1, n):
+            dp[i][0] = max(dp[i - 1][0], dp[i - 1][2] - prices[i])
+            dp[i][1] = dp[i - 1][0] + prices[i]
+            dp[i][2] = max(dp[i - 1][1], dp[i - 1][2])
+
+        return max(dp[n - 1][1], dp[n - 1][2])
