ae1dab326632178d363e3ee8e4f7956b1c9946cd
cj <616151263@qq.com>
2020-07-14 18:24:33 +0800

    update

--- leetcode.py
+++ leetcode.py
@@ -134,17 +134,49 @@ import collections
 # S =Solution()
 # res = S.divingBoard(1, 1, 0)
 # print(res)
+# class Solution:
+#     def maxProfit(self, prices):
+#         if not prices:
+#             return 0
+#
+#         n = len(prices)
+#         dp = [[-prices[0], 0, 0]] + [[0] * 3 for _ in range(n - 1)]  # 构建dp数组存储状态
+#
+#         for i in range(1, n):
+#             dp[i][0] = max(dp[i - 1][0], dp[i - 1][2] - prices[i])
+#             dp[i][1] = dp[i - 1][0] + prices[i]
+#             dp[i][2] = max(dp[i - 1][1], dp[i - 1][2])
+#
+#         return max(dp[n - 1][1], dp[n - 1][2])
+
+### leetcode120 三角形最小路径和
+
 class Solution:
-    def maxProfit(self, prices):
-        if not prices:
-            return 0
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
 
-        n = len(prices)
-        dp = [[-prices[0], 0, 0]] + [[0] * 3 for _ in range(n - 1)]  # 构建dp数组存储状态
+        n = len(triangle)
+        dp = [[0] * n for _ in range(n)]
+        dp[0][0] = triangle[0][0]
 
+        # dp[0] = triangle[0][0]
         for i in range(1, n):
-            dp[i][0] = max(dp[i - 1][0], dp[i - 1][2] - prices[i])
-            dp[i][1] = dp[i - 1][0] + prices[i]
-            dp[i][2] = max(dp[i - 1][1], dp[i - 1][2])
+            dp[i][0] = dp[i - 1][0] + triangle[i][0]
+            for j in range(1, i):
+                dp[i][j] = min(dp[i - 1][j], dp[i - 1][j - 1]) + triangle[i][j]
+            dp[i][i] = dp[i - 1][i - 1] + triangle[i][i]
 
-        return max(dp[n - 1][1], dp[n - 1][2])
+        return min(dp[n - 1])
+
+
+
+if __name__ == '__main__':
+    triangle = [[2],[3,4],[6,5,7],[4,1,8,3]]
+    S = Solution()
+    re = S.minimumTotal(triangle)
+    print(re)
+    # [
+    #     [2],
+    #    [3, 4],
+    #   [6, 5, 7],
+    #  [4, 1, 8, 3]
+    # ]
